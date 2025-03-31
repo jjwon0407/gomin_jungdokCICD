b9766e8e754021c7ad86151413e95395d141261b
jjwon0407 <jjwon0407@naver.com>
2025-03-31 23:00:46 +0900

    Fix: application.properties, GdgocApplicationTests.java 수정

--- backend/src/test/java/com/gomin_jungdok/gdgoc/GdgocApplicationTests.java
+++ backend/src/test/java/com/gomin_jungdok/gdgoc/GdgocApplicationTests.java
@@ -3,6 +3,14 @@ package com.gomin_jungdok.gdgoc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@TestPropertySource(properties = {
+    "spring.datasource.url=${DB_URL}",
+    "spring.datasource.username=${DB_USERNAME}",
+    "spring.datasource.password=${DB_PASSWORD}",
+    "spring.cloud.gcp.storage.bucket=${GCS_NAME}",
+    "spring.cloud.gcp.storage.credentials.location=${GCS_PATH}"
+})
+
 @SpringBootTest
 class GdgocApplicationTests {
 
