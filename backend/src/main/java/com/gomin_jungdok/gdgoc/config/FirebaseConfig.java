358709f485740e00cd39baa39a9d3962e1fba14b
jjwon0407 <jjwon0407@naver.com>
2025-04-10 22:06:50 +0900

    Fix: Value 어노테이션 import

--- backend/src/main/java/com/gomin_jungdok/gdgoc/config/FirebaseConfig.java
+++ backend/src/main/java/com/gomin_jungdok/gdgoc/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
